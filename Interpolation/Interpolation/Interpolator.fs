798971cc9b53bb5cad2ca215abfa7c289b78db49
Inzilbeth <talgat.minkashev@yandex.com>
2021-09-23 23:25:44 +0300

    Implement main structure and Lagrange interpolation

---
+++ Interpolation/Interpolation/Interpolator.fs
@@ -0,0 +1,33 @@
+﻿namespace Interpolation
+
+type public Interpolator() =
+
+    member public Interpolator.getValues (a : double) (b : double) (m : double) (f : IFunction) =
+        let rec getValuesLoop nodes values (j : int) =
+            match j with
+            | j when (float (j)) < m -> let xj = a + (float (j)) * (b - a) / m
+                                        getValuesLoop (xj :: nodes) ((f.compute(xj)) :: values) (j + 1)
+
+            | _ -> List.zip nodes values
+
+        getValuesLoop [] [] 0
+
+    member public Interpolator.prepareTable table x n =
+        List.sortBy (fun (node, value) -> abs (node - x)) table |> List.take (n + 1)
+
+    member public Interpolator.interpolateLagrange table x =
+        let rec interpolationLoop list sum =
+            let calculateDenominator kthNode =
+                List.fold (fun acc (node, value) -> if node = kthNode then acc else acc * (kthNode - node)) 1.0 table
+
+            let calculateNumerator kthNode =
+                List.fold (fun acc (node, value) -> if node = kthNode then acc else acc * (x - node)) 1.0 table
+
+            match list with
+            | (node, value) :: tail -> interpolationLoop tail ((calculateNumerator node) / (calculateDenominator node) * value + sum)
+            | [] -> sum
+
+        interpolationLoop table 0.0
+
+    member public Interpolator.interpolateNewton table x =
+        0
---
+++ Interpolation/Interpolation/SimpleFunction.fs
@@ -0,0 +1,10 @@
+﻿namespace Interpolation
+
+type public FirstFunction() =
+
+    interface IFunction with
+        member this.Name =
+            "sqrt(1 + x^2)"
+
+        member this.compute x =
+            1.0 + x ** 2.0 |> sqrt
